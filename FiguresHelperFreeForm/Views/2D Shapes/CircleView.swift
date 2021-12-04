bf58748f5180ba2a90a417ca2d74bc66874030ce
Joyce Tao <jtao@lcs.on.ca>
2021-12-04 10:51:57 -0500

    Changed from using a Slider to a TextField.
    Applied the use of optional properties. Used "guard" statements to check for valid input.
    
    When input was not valid, we returned nil.

--- FiguresHelperFreeForm/Views/2D Shapes/CircleView.swift
+++ FiguresHelperFreeForm/Views/2D Shapes/CircleView.swift
@@ -10,10 +10,42 @@ import SwiftUI
 struct CircleView: View {
     
     // MARK: Stored properties
-    @State var radius = 10.0
+    @State var providedRadius = ""
     
     // MARK: Computed properties
-    var area: Double {
+    
+    // Check the input given by the user
+    // If possible, returns a Double
+    // Otherwise, returns nil
+    var radius: Double? {
+        
+        // 1. Ensure that we can change the input (String) to a Double
+        // 2. Ensure that the value is more than zero
+        // To do this, we use a "guard" statement
+        guard let radius = Double(providedRadius),
+              radius > 0
+        else {
+            // When either of the tests above do not pass, then we don't have a valid radius
+            return nil
+        }
+        
+        // If we get here, we know the radius is valid
+        return radius
+        
+    }
+    
+    // Attempts to calculate the area - if it cannot, return nil
+    var area: Double? {
+        
+        // Are the input(s) valid?
+        // The "black" radius is a local constant
+        // It only exists AFTER the guard statement
+        guard let radius = radius
+        else {
+            // Input(s) not valid, so we cannot compute the area
+            return nil
+        }
+        
         return Double.pi * radius * radius
     }
     
@@ -28,23 +60,9 @@ struct CircleView: View {
                 SectionLabelView(text: "Radius", variable: "r")
 
                 // Input: Radius
-                Slider(value: $radius,
-                       in: 0.0...100.0,
-                       step: 0.1,
-                       label: {
-                    Text("Radius")
-                },
-                       minimumValueLabel: {
-                    Text("0")
-                },
-                       maximumValueLabel: {
-                    Text("100")
-                })
-                
-                // Output: Radius
-                SliderValueView(value: radius)
-                
-                SectionLabelView(text: "Area", variable: "")
+                TextField("Radius",
+                          text: $providedRadius,
+                          prompt: Text("Numeric value greater than 0"))
                 
                 // Output: Area
                 OutputValueView(value: area, suffix: "square units")
--- FiguresHelperFreeForm/Views/Helpers/OutputValueView.swift
+++ FiguresHelperFreeForm/Views/Helpers/OutputValueView.swift
@@ -10,16 +10,30 @@ import SwiftUI
 struct OutputValueView: View {
     
     // MARK: Stored properties
-    let value: Double
+    let value: Double?
     let suffix: String
     
     // MARK: Computed properties
-    var formattedValue: String {
-        return String(format: "%.1f", value)
+    
+    // When there is a value to show, present it in a nicely formatted manner
+    // When there is NO value to show (nil), we will show an appropriate error message
+    var output: String {
+        
+        // Is there valid input to show?
+        if let value = value {
+            // Create a local String that is the formatted value
+            let formattedValue = String(format: "%.1f", value)
+            
+            return "\(formattedValue) \(suffix)"
+            
+        } else {
+            return "Cannot currently be computed."
+        }
+        
     }
     
     var body: some View {
-        Text("\(formattedValue) \(suffix)")
+        Text(output)
             .font(.title3)
             .bold()
     }
@@ -27,7 +41,12 @@ struct OutputValueView: View {
 
 struct OutputValueView_Previews: PreviewProvider {
     static var previews: some View {
+        
         OutputValueView(value: 12131.121,
                         suffix: "square units")
+        
+        OutputValueView(value: nil,
+                        suffix: "square units")
+        
     }
 }
